de95ccb9f30778dc07dd41f7e89fc0d8fba75d1b
lm7-1 <larryamercier@gmail.com>
2025-11-19 08:51:27 -0500

    updated manual turret moving. fixed flywheel speed increment adjustments, removed gamepad2 heading control, updated blue_rear12 auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/TurretAiming.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/TurretAiming.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.robot;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.util.Timer;
 import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -13,6 +14,8 @@ public class TurretAiming {
     private final servos Servos;
     private final motors robotMotors;
     private final Telemetry telemetry;
+
+    private final other_helpers helpers = new other_helpers();
 
     private final other_helpers headingAverage = new other_helpers();
     private final other_helpers distanceAverage = new other_helpers();
@@ -27,6 +30,7 @@ public class TurretAiming {
     private int shooterSpeedAdjust = 0;
     private double turretPosAdjust = 0;
     private static final int ticks_per_rev = 28;
+    private boolean isManualTurret = false;
 
     public TurretAiming(Follower follower, limelight3A limelight, servos Servos, motors robotMotors, Telemetry telemetry) {
         this.follower = follower;
@@ -38,6 +42,16 @@ public class TurretAiming {
         headingAverage.initMovingAverage(3);
         distanceAverage.initMovingAverage(3);
     }
+
+    public boolean isManualTurret() {
+        return this.isManualTurret;
+    }
+
+    public void setManualTurret(boolean isManualTurret) {
+        this.isManualTurret = isManualTurret;
+    }
+
+
 
     public void update(String myAllianceColor) {
         if (llTimer.getElapsedTime() >= 10) {
@@ -54,7 +68,7 @@ public class TurretAiming {
 
             } else {
                 Servos.setLedColor(servos.LedColor.RED);
-                if (targetTimer.getElapsedTime() >= 1000) {
+                if (targetTimer.getElapsedTime() >= 500) {
                     target_acquired = false;
                     targetTimer.resetTimer();
                 }
@@ -71,10 +85,10 @@ public class TurretAiming {
             }
 
             // Set shooter velocity
-            if (goalDistance > 0 && goalDistance < 5) {
+            if (goalDistance > .1 && goalDistance < 5) {
                 double targetRPM = other_helpers.FlywheelShooter.getRPMForDistance(goalDistance);
-                speed = targetRPM * ticks_per_rev / 60;
-                robotMotors.setShooterVelocity(speed + shooterSpeedAdjust);
+                speed = Range.clip((targetRPM + shooterSpeedAdjust) * ticks_per_rev / 60, helpers.MIN_RPM, helpers.MAX_RPM);
+                robotMotors.setShooterVelocity(speed);
             }
 
             // Adjust turret using PID
