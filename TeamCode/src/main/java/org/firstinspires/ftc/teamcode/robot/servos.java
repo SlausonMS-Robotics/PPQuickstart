aa67a185c9a05a7ed99e901ac8f5f3507ce565d9
lm7-1 <larryamercier@gmail.com>
2025-11-25 15:40:55 -0500

    lots of updates, simplifying as mush as possible

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/servos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/servos.java
@@ -11,35 +11,35 @@ public class servos {
 
     public static final double TURRET_MIN_POS = .3;
     public static final double TURRET_MAX_POS = .7;
-    public static final double MANUAL_TURRET_SPEED = 0.005; // Adjust this for manual control sensitivity
 
     // ---- PID Constants ----
     // NOTE: These will need to be tuned for your specific robot
-    public static final double TURRET_P = 0.5;
+    public static final double TURRET_P = 0.9;
     public static final double TURRET_I = 0.0;
-    public static final double TURRET_D = 0.0;
+    public static final double TURRET_D = 0.3;
 
     // ---- PID Controller ----
     private other_helpers pidController = new other_helpers();
 
     // ---- Servos ----
-    private ServoImplEx turretServo;     // servo0
-    private ServoImplEx indexerServo;  // servo1
-    private ServoImplEx ledServo;      // servo2 for LED control
+    private ServoImplEx turretServo, ledServo, intakeServo1, intakeServo2;
 
-    public enum LedColor { GREEN, RED, OFF }
+
+    public enum LedColor { GREEN, RED, VIOLET, OFF }
 
     /**
      * Initializes all servos and the PID controller for the turret.
      */
     public void init(HardwareMap hardwareMap) {
-        turretServo = hardwareMap.get(ServoImplEx.class, "servo0");
-        indexerServo = hardwareMap.get(ServoImplEx.class, "servo1");
-        ledServo = hardwareMap.get(ServoImplEx.class, "led");
+        turretServo = hardwareMap.get(ServoImplEx.class, "shservo0");
+        ledServo = hardwareMap.get(ServoImplEx.class, "shservo1");
+        intakeServo1 = hardwareMap.get(ServoImplEx.class, "shservo3");
+        intakeServo2 = hardwareMap.get(ServoImplEx.class, "shservo4");
+
         ledServo.setPwmRange(new PwmControl.PwmRange(500, 2500));
 
         // Center the turret on initialization
-        setTurretServoPos(0.5);
+        //setTurretServoPos(0.5);
 
         // Initialize the PID controller with our constants
         pidController.initPID(TURRET_P, TURRET_I, TURRET_D);
@@ -48,14 +48,17 @@ public class servos {
     public void setLedColor(LedColor color) {
         switch (color) {
             case GREEN:
-                ledServo.setPosition(1.0);
+                ledServo.setPosition(.5);
                 break;
             case RED:
-                ledServo.setPosition(0.0);
+                ledServo.setPosition(0.28);
+                break;
+            case VIOLET:
+                ledServo.setPosition(0.72);
                 break;
             case OFF:
             default:
-                ledServo.setPosition(0.5);
+                ledServo.setPosition(0.0);
                 break;
         }
     }
@@ -70,32 +73,38 @@ public class servos {
         setTurretServoPos(newPos);
     }
 
+    public void setIntakeServos(boolean on){
+        if(on){
+            intakeServo1.setPosition(1);
+            intakeServo2.setPosition(0);
+        }
+        else{
+            intakeServo1.setPosition(.5);
+            intakeServo2.setPosition(.5);
+        }
+    }
+
     /**
      * Updates the turret position using a PID controller to minimize heading error.
-     * @param headingError The error in radians between the current and target heading.
+     * @param headingErrorDeg The error in radians between the current and target heading.
      */
-    public void updateTurretWithPID(double headingError) {
-        double pidCorrection = pidController.updatePID(headingError, 0);
+    public void updateTurretWithPID(double headingErrorDeg) {
+        double pidCorrection = pidController.updatePID(headingErrorDeg, 0);
         double currentPos = getTurretServoPos();
         double newPos = currentPos + pidCorrection;
         setTurretServoPos(newPos);
     }
 
     public void setTurretServoPos(double pos) {
-        if (turretServo != null) {
-            turretServo.setPosition(Range.clip(pos, TURRET_MIN_POS, TURRET_MAX_POS));
-        }
+        turretServo.setPosition(Range.clip(pos, TURRET_MIN_POS, TURRET_MAX_POS));
     }
 
     public double getTurretServoPos() {
-        if (turretServo != null) {
-            return turretServo.getPosition();
-        }
-        return 0.5; // Default to center
+        return turretServo.getPosition();
     }
 
-    public void setIndexerServoPos(double pos) {
-        if (indexerServo != null) indexerServo.setPosition(pos);
-    }
+
+
+
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/AutoAimTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/AutoAimTeleop.java
@@ -11,8 +11,8 @@ import org.firstinspires.ftc.teamcode.robot.PoseStorage;
 import org.firstinspires.ftc.teamcode.robot.TurretAiming;
 import org.firstinspires.ftc.teamcode.robot.limelight3A;
 import org.firstinspires.ftc.teamcode.robot.motors;
-import org.firstinspires.ftc.teamcode.robot.other_helpers;
 import org.firstinspires.ftc.teamcode.robot.servos;
+import org.firstinspires.ftc.teamcode.robot.states;
 
 /**
  * This is an example TeleOp for testing out the 2360 robot. It'll need to be updated to allow for
@@ -27,13 +27,21 @@ public class AutoAimTeleop extends OpMode {
     motors robotMotors = new motors(); // Single object for all motors
     limelight3A limelight = new limelight3A();
     servos Servos = new servos();
+
+    states robotStateController = new states();
+
+
     private TurretAiming turretAimer;
+    private int robotState = 0;
+    private int previousRobotState = 0;
+    private int intakeState = 0;
+    private int transferState = 0;
+    private Pose startPose;
 
 
     Follower follower;
 
-    private int shooterSpeedAdjust = 0;
-    private double turretPosAdjust = 0;
+
 
     /** This method is call once when init is played, it initializes the follower **/
     @Override
@@ -41,39 +49,36 @@ public class AutoAimTeleop extends OpMode {
         // Initialize all our robot hardware
         robotMotors.init(hardwareMap); //initializes all motors
         Servos.init(hardwareMap); // This now initializes the turret servo and PID
-        limelight.init(hardwareMap,0, telemetry);
 
         follower = Constants.createFollower(hardwareMap);
-        Pose startPose = PoseStorage.currentPose;
-        follower.setStartingPose(startPose);
-        telemetry.addData("Starting Pose", startPose);
+
 
         // Instantiate our new reusable classes
         turretAimer = new TurretAiming(follower, limelight, Servos, robotMotors, telemetry);
-
 
         llPoseTimer = new Timer();
         buttonDebounceTimer = new Timer();
         
         myAllianceColor = PoseStorage.allianceColor;
+        if("blue".equals(myAllianceColor)){
+            limelight.init(hardwareMap,0, telemetry);
+            startPose = new Pose(44,9,Math.toRadians(90));
+
+        }
+        else {
+            limelight.init(hardwareMap,1, telemetry);
+            startPose = new Pose(72,9,Math.toRadians(90));
+        }
+        follower.setStartingPose(startPose);
         telemetry.addData("Alliance Color", myAllianceColor);
+        telemetry.addData("Starting Pose", startPose);
         telemetry.update();
     }
 
     /** This method is called continuously after Init while waiting to be started. **/
     @Override
     public void init_loop() {
-        if(buttonDebounceTimer.getElapsedTime() > 500 && (gamepad1.start || gamepad2.start)){
-            buttonDebounceTimer.resetTimer();
-            if("blue".equals(myAllianceColor)){
-                myAllianceColor = "red";
-            } else {
-                myAllianceColor = "blue";
-            }
-            telemetry.addData("Alliance Color", myAllianceColor);
-            telemetry.update();
-            PoseStorage.allianceColor = myAllianceColor;
-        }
+
     }
 
     /** This method is called once at the start of the OpMode. **/
@@ -89,67 +94,43 @@ public class AutoAimTeleop extends OpMode {
     @Override
     public void loop() {
 
-        if (gamepad2.right_trigger > .2) { //transfer on/off (shoot)
-            robotMotors.shoot();
-        } else {
-            robotMotors.setTransfer(false); //turn transfer off
-        }
 
-        // Manual turret control with right joystick
-        if (Math.abs(gamepad2.right_stick_x) > 0.1) {
-            Servos.moveTurretManually(gamepad2.right_stick_x / 100);
-            turretAimer.setManualTurret(true);
+        if (gamepad2.right_trigger > .2) { //transfer on/off (shoot)
+            if (robotState != 2) previousRobotState = robotState;
+            robotState = 2;
+
         }
-        else {
-            turretAimer.setManualTurret(false);
-        }
+        else robotState = previousRobotState;
 
         // Gamepad button logic for adjustments and intake
-        if (buttonDebounceTimer.getElapsedTime() >= 250) { //debounce all buttons
-            if (other_helpers.anyButtonPressed(gamepad2)) {
-                int shooterSpeedAdjustIncrement = 50;
-                if (gamepad2.dpad_up) {
-                    shooterSpeedAdjust += shooterSpeedAdjustIncrement;
-                }
-                if (gamepad2.dpad_down) {
-                    shooterSpeedAdjust -= shooterSpeedAdjustIncrement;
-                }
+        if (buttonDebounceTimer.getElapsedTime() >= 350) { //debounce all buttons
 
-
-
-                if (gamepad2.a || gamepad2.y) { //intake on/off
-                    robotMotors.setIntake(!robotMotors.isIntakeOn()); //toggle intake
-                }
+            if (gamepad2.a || gamepad2.y) { //intake on/off
+                if(robotState == 0) robotState = 1;
+                else robotState = 0;
                 buttonDebounceTimer.resetTimer();
+
             }
         }
 
-        // Pass manual adjustments to the TurretAiming class
-        if (turretAimer != null) {
-            turretAimer.setShooterSpeedAdjust(shooterSpeedAdjust);
-            turretAimer.setTurretPosAdjust(turretPosAdjust);
+        // Drive code
+        double scalar;
+        if (gamepad1.left_trigger > .2) { //driver
+            scalar = .5; //sets speed of change -> lower = slower
+            follower.setTeleOpDrive(Math.pow(-gamepad1.left_stick_y * scalar, 1), Math.pow(-gamepad1.left_stick_x * scalar, 1), Math.pow(-gamepad1.right_stick_x * scalar, 1), false);
+        } else {
+            scalar = 1.0; //sets speed of change -> lower = slower
+            follower.setTeleOpDrive(Math.pow(-gamepad1.left_stick_y * scalar, 1), Math.pow(-gamepad1.left_stick_x * scalar, 1), Math.pow(-gamepad1.right_stick_x * scalar, 1), false);
         }
 
 
-            // Drive code
-            double scalar;
-            if (gamepad1.left_trigger > .2 || gamepad2.left_trigger > .2) { //driver
-                scalar = .5; //sets speed of change -> lower = slower
-                follower.setTeleOpDrive(Math.pow(-gamepad1.left_stick_y * scalar, 1), Math.pow(-gamepad1.left_stick_x * scalar, 1), Math.pow(-gamepad1.right_stick_x * scalar, 1), false);
-            } else {
-                scalar = 1.0; //sets speed of change -> lower = slower
-                follower.setTeleOpDrive(Math.pow(-gamepad1.left_stick_y * scalar, 1), Math.pow(-gamepad1.left_stick_x * scalar, 1), Math.pow(-gamepad1.right_stick_x * scalar, 1), false);
-            }
-            
-            // Update odometry and sensor fusion
-            follower.update();
+        turretAimer.update(myAllianceColor, true);
+        robotStateController.setRobotState(robotState);
+        follower.update();
 
-            // Update turret aiming logic
-            if (turretAimer != null){
-                if (!turretAimer.isManualTurret()) {
-                    turretAimer.update(myAllianceColor);
-                }
-            }
+
+
+
 
     }
 
